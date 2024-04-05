0bc897b89a6889c14d64ed8e695824ef1c7a12c7
junglesmay <104856219+junglesmay@users.noreply.github.com>
2024-04-04 22:43:47 -0300

    Update main.js

--- main.js
+++ main.js
@@ -13,3 +13,9 @@ for(let i=0;i <botoes.length;i++){
     textos[i].classlist.add("ativo");
   }
 }
+
+const contadores = document.querySelectorAll(".contador");
+const tempoObjetivo1 = new Date("2024-10-05T00:00:00")
+let tempoAtual = new Date();
+
+contadores[o].textContent = tempoObjetivo1 - tempoAtual;
