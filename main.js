ae29cadb93418451acc0cefb4ac361903d8a5a34
junglesmay <104856219+junglesmay@users.noreply.github.com>
2024-04-04 22:24:03 -0300

    Update main.js

--- main.js
+++ main.js
@@ -1,13 +1,15 @@
 const botoes = document.querySelectorAll(".botao");
-
+const textos = document.querySelectorAll(".aba-conteudo");
+  
 for(let i=0;i <botoes.length;i++){
   botoes[i].onclick = function(){
 
     for(let j=0;j<botoes.length;j++){
       botoes[j].classlist.remove("ativo");
+      textos[j],classlist.remove("ativo");
     }
     
     botoes[i].classlist.add("ativo");
+    textos[i].classlist.add("ativo");
   }
-  console.log();
 }
